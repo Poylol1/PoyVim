8f135e92179cca92eaefe92bdea8e4e22118969c
poylol <poylolt@gmail.com>
2024-07-22 16:38:57 -0700

    questions?

---
+++ lua/LSP/masonI.lua
@@ -0,0 +1,25 @@
+return {
+  ['basicMason'] = {
+    'williamboman/mason.nvim',
+    config = function()
+      require('mason').setup()
+    end,
+  },
+  ['masonlspConfig'] = {
+    'williamboman/mason-lspconfig.nvim',
+    config = function()
+      require('mason-lspconfig').setup {
+        ensure_installed = require('LSP.LSPconfig')['mason'],
+      }
+
+      local lspconfig = require 'lspconfig'
+      local mason_lspconfig = require 'mason-lspconfig'
+
+      mason_lspconfig.setup_handlers {
+        function(server_name)
+          lspconfig[server_name].setup {}
+        end,
+      }
+    end,
+  },
+}
