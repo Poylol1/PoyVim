d6440e02a2c9423b8ab61a783aa5e4f12d4ead37
poylol <poylolt@gmail.com>
2024-08-12 19:48:26 -0700

    New Commit!!
    idk

--- lua/LSP/vimtexI.lua
+++ lua/LSP/vimtexI.lua
@@ -3,10 +3,17 @@ return {
   lazy = false, -- we don't want to lazy load VimTeX
   tag = 'v2.15', -- uncomment to pin to a specific release
   init = function()
+    Current = 0
     -- VimTeX configuration goes here, e.g.
     -- Viewer options: One may configure the viewer either by specifying a built-in
     -- viewer method:
     vim.g.vimtex_view_method = 'zathura'
+
+    vim.cmd 'set shiftwidth=2'
+    vim.cmd 'set tabstop=2'
+
+    vim.cmd 'set conceallevel=1'
+    vim.cmd 'let g:tex_conceal="abdmgs"'
 
     -- Or with a generic interface:
     vim.g.vimtex_view_general_viewer = 'okular'
@@ -18,40 +25,91 @@ return {
     -- supported backends and further explanation is provided in the documentation,
     -- see ":help vimtex-compiler".
     vim.g.vimtex_compiler_method = 'latexmk'
-    -- stops the latex sesison
 
+    -- simple utils functions
+    function Split(str, divider)
+      Turner = {}
+      Currentstr = ''
+      for i = 1, #str do
+        local currentchar = str:sub(i, i)
+        if divider ~= currentchar then
+          Currentstr = Currentstr .. currentchar
+        else
+          table.insert(Turner, Currentstr)
+          Currentstr = ''
+        end
+      end
+      table.insert(Turner, Currentstr)
+      return Turner
+    end
+
+    function Getfilename(testpat)
+      for i = #testpat, 1, -1 do
+        if testpat:sub(i, i) == '/' then
+          return string.sub(testpat, i + 1, -5)
+        end
+      end
+      return error 'invalid string'
+    end
+
+    function Getrelativepath(str, objective, side)
+      for i = 1, #str - #objective, 1 do
+        if str:sub(i, i + #objective - 1) == objective then
+          if side == 1 then
+            return str:sub(i + #objective)
+          end
+          if side == -1 then
+            return str:sub(1, i - 2)
+          end
+          return str:sub(i)
+        end
+      end
+      return error 'invalid string'
+    end
+
+    -- stops the latex sesison
     function Run_cleartex()
+      BufferName = vim.api.nvim_buf_get_name(Current)
       -- Get the current buffer name
-      local file_path = string.sub(vim.api.nvim_buf_get_name(0), 1, -5)
+      local file_path = string.sub(BufferName, 1, -5)
       --extensions for lua
       local extensions = { 'aux', 'fdb_latexmk', 'fls', 'log', 'synctex.gz' }
-      -- command to stop latex compilation
-      vim.cmd 'VimtexStopAll'
       -- removes all trash files
+      print(file_path .. '.file')
       for i = 1, #extensions do
         os.remove(file_path .. '.' .. extensions[i])
       end
-      -- moves the pdf file to the pdf directory
-      os.execute('mv ' .. file_path .. '.pdf ' .. '../pdf/')
+      -- moves the pdf file to the pdf directory'
+      -- ~2/3D4o5c6u7m8e9n10t11s12/13c14a15l16c17_18n19o20t21e22s23/24l25a26t27e28x29/30
+      local relativepath = Getrelativepath(BufferName, 'latex', 1)
+      local pdfpath = Split(relativepath, '/')
+
+      local buildingpath = Getrelativepath(BufferName, 'latex', -1) .. '/pdf'
+      local filename = Getfilename(BufferName)
+      for i = 1, #pdfpath - 1 do
+        buildingpath = buildingpath .. '/' .. pdfpath[i]
+        os.execute('if ! [ -d ' .. buildingpath .. ' ]; then mkdir ' .. buildingpath .. ' ; fi')
+      end
+
+      os.execute('mv ' .. file_path .. '.pdf ' .. buildingpath .. '/' .. filename .. '.pdf')
       -- closes zathura
       os.execute 'pkill -f zathura'
     end
     -- util to simpler commands
     local function texcommands(key, action, opts)
-      vim.api.nvim_buf_set_keymap(vim.api.nvim_get_current_buf(), 'n', key, action, opts)
+      vim.api.nvim_buf_set_keymap(Current, 'n', key, action, opts)
     end
     -- actual commands
     local function vimtex_keymaps()
       texcommands('\\c', '<ESC>:VimtexCompile<CR>', { desc = '[C]ompiles the file' })
 
       texcommands('\\k', '', { noremap = true, silent = true, callback = Run_cleartex, desc = '[K]ills the compilation' })
-
-      texcommands('\\p', ':lua print("this works")', {})
     end
     -- creates autogroup for second autocommand
     vim.api.nvim_create_augroup('TexKeymaps', { clear = true })
     -- sets the special keymaps
     vim.api.nvim_create_autocmd('FileType', { pattern = 'tex', callback = vimtex_keymaps, group = 'TexKeymaps' })
+<<<<<<< HEAD
 
     --vim.api.nvim_create_autocmd('FileType', {
     --  pattern = 'tex',
@@ -66,15 +124,23 @@ return {
     --    if vim.api.nvim_buf_get_name(0):match '%.tex$' then
     --      vim.cmd 'VimtexCompile'
     --   end
+=======
+    --opens stuff
+    --vim.api.nvim_create_autocmd('BufEnter', {
+    --  callback = function()
+    --    if vim.api.nvim_buf_get_name(Current):match '%.tex$' then
+    --      vim.cmd 'VimtexCompile'
+    --    end
+>>>>>>> 024374e (New Commit!!)
     --  end,
     --})
     -- closes stuff
-    vim.api.nvim_create_autocmd({ 'BufLeave', 'VimLeave' }, {
-      callback = function()
-        if vim.api.nvim_buf_get_name(0):match '%.tex$' then
-          Run_cleartex()
-        end
-      end,
-    })
+    --vim.api.nvim_create_autocmd({ 'BufLeave', 'VimLeave' }, {
+    --callback = function()
+    --if vim.api.nvim_buf_get_name(Current):match '%.tex$' then
+    --Run_cleartex()
+    --end
+    --end,
+    --})
   end,
 }
--- lua/starter/lazyconfig.lua
+++ lua/starter/lazyconfig.lua
@@ -136,20 +136,17 @@ require('lazy').setup({
       require('which-key').setup()
 
       -- Document existing key chains
-      require('which-key').register {
-        ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-        ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-        ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-        ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-        ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
-        ['<leader>g'] = { name = '[G]oto', _ = 'which_key_ignore' },
+      require('which-key').add {
+        { '<leader>c', group = '[C]ode' },
+        { '<leader>d', group = '[D]ocument' },
+        { '<leader>r', group = '[R]ename' },
+        { '<leader>s', group = '[S]earch' },
+        { '<leader>w', group = '[W]orkspace' },
+        { '<leader>t', group = '[T]oggle' },
+        { '<leader>h', group = 'Git [H]unk' },
+        { '<leader>g', group = '[G]oto' },
       }
       -- visual mode
-      require('which-key').register({
-        ['<leader>h'] = { 'Git [H]unk' },
-      }, { mode = 'v' })
     end,
   },
 
@@ -169,7 +166,18 @@ require('lazy').setup({
 
   require 'LSP.autoformatI',
 
-  { 'L3MON4D3/LuaSnip', run = 'make install_jsregexp' },
+  --NOTE: Until I can make this work with LuaSnip then I will uncomment
+  --this
+  --
+  --require 'LSP.cocI',
+
+  {
+    'L3MON4D3/LuaSnip',
+    -- follow latest release.
+    version = 'v2.*', -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+    -- install jsregexp (optional!).
+    build = 'make install_jsregexp',
+  },
 
   require 'LSP.autocompleteI',
 
